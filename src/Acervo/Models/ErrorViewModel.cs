2329e4c3a6385e7e5c0168a2de6202fe567cfaf3
DESKTOP-IJRAL9A\dudin <mariaedealmeida7@gmail.com>
2023-04-04 12:53:48 -0300

    Projeto adicionado

---
+++ src/Acervo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Acervo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
