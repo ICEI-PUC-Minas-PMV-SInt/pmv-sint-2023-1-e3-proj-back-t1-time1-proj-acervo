5b544ac095fde2b02d9c47f39515dd384d6e14c9
Leticia.Lopes <leticia.rosedesantana@gmail.com>
2023-04-16 21:48:04 -0300

    Criacao da view de Cadastro dos Livros

--- src/Acervo/Controllers/CatalogoController.cs
+++ src/Acervo/Controllers/CatalogoController.cs
@@ -18,6 +18,11 @@ namespace Acervo.Controllers
             return View();
         }
 
+        public IActionResult Register()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
