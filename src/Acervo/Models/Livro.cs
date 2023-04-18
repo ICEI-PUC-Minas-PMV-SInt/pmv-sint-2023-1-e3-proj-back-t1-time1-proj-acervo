4b2fc159904502e961295175ad6f45942563f63e
DESKTOP-IJRAL9A\dudin <mariaedealmeida7@gmail.com>
2023-04-17 22:25:35 -0300

    Modelos adicionados

---
+++ src/Acervo/Models/Livro.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System;
+
+namespace Acervo.Models
+{
+    public class Livro
+    {
+        [Key, MaxLength(30)]
+        public Guid LivroId { get; set; }
+        [Required]
+        public string Titulo { get; set; }
+        [Required]
+        public string Tipo { get; set; }
+        [Required]
+        public string Autor { get; set; }
+        [Required]
+        public string Localização { get; set; }
+        [Required]
+        public string AnoPublicação { get; set; }
+        [Required]
+        public bool? Reservado { get; set; }
+    }
+}
