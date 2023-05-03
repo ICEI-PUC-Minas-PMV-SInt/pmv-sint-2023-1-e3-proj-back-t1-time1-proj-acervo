8e1ee7c9802092bb5f01ad24c4ee7f30bd6c8be3
DESKTOP-IJRAL9A\dudin <mariaedealmeida7@gmail.com>
2023-05-02 22:03:31 -0300

    DB context in progress

---
+++ src/Acervo/Models/AplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks
+
+namespace Acervo.Models
+{
+	public ApplicationDbContext : DbContext
+	{
+	}
+}
