6dfed7389cd232a604f476da49a177c6a11ac4c9
Maria Almeida <mariaedealmeida7@gmail.com>
2023-06-04 21:16:51 -0300

    Login e autenticação de usuario

---
+++ src/BuscaBook/Models/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BuscaBook.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public Guid UsuarioId { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a senha")]
+        [DataType(DataType.Password)]
+        public string? Senha { get; set; }
+        public Perfil Perfil { get; set; }
+    }
+
+    public enum Perfil
+    {  Admin,
+       Usuario,
+    }
+
+}
